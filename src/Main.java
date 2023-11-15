3d2e2e91dc021e52d0bd0fae7610984ddd9359a0
JoannnW <joanw2026@student.cis.edu.hk>
2023-11-15 09:00:40 +0800

    finished

--- src/Main.java
+++ src/Main.java
@@ -8,9 +8,16 @@ public class Main {
      * zip(myArray1, myArray2) → {1,3,5,7,9,2,4,6,8,10,12,14,16}
      */
     public static int[] combine(int[] array1, int[] array2) {
-
-        return null;
-
+        int[] myArray;
+        int length=array1.length + array2.length;
+        myArray=new int[length];
+        for (int k : array1) {
+            myArray[k] = array1[k];
+        }
+        for (int j : array2) {
+            myArray[array1.length+j] = array2[j];
+        }
+        return myArray;
     }
 
     /**
@@ -21,9 +28,13 @@ public class Main {
      * zip(myArray1, myArray2) → {1,2,3,4,5,6,7,8,9,10}
      */
     public static int[] zip(int[] array1, int[] array2) {
-
-        return null;
-
+        int[] myArray;
+        myArray=new int[array1.length*2];
+        for(int i=0;i< myArray.length;i+=2){
+            myArray[i]=array1[i];
+            myArray[i+1]=array2[i];
+        }
+        return myArray;
     }
 
     /**
@@ -34,9 +45,13 @@ public class Main {
      * product(myArray1, myArray2) → {2,12,30,56,90}
      */
     public static int[] product(int[] array1, int[] array2) {
-
-        return null;
-
+        int[] myArray;
+        int length=array1.length;
+        myArray=new int[length];
+        for(int i=0;i<array1.length;i++){
+            myArray[i]=array1[i]*array2[i];
+        }
+        return myArray;
     }
 
     /**
@@ -56,15 +71,22 @@ public class Main {
      * capitalCount(words) → {1, 2, 2, 0}
      */
     public static int[] capitalCount(String[] words) {
-
-        return null;
-
+        int[] myArray;
+        myArray=new int[words.length];
+        for(int i=0;i< words.length;i++){
+            myArray[i]= countCapitalLetters(words[i]);
+        }
+        return myArray;
     }
 
     public static int countCapitalLetters(String word) {
-
-        return 0;
-
+        int count=0;
+        for(int i=0;i<word.length();i++){
+            if (Character.isAlphabetic(i)){
+                count++;
+            }
+        }
+        return count;
     }
 
 }
